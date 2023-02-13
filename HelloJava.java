26e14147a955428815661abd370b2825239abcc5
Kai Schell <kai.schell@signet.de>
2023-02-13 13:18:32 +0100

    Einfaches Hallo Welt Programm

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        
+        System.out.println("1. Argument: " + args[0]);
+        System.out.println("2. Argument: " + args[1]);
+
+    }
+
+}
+
+// print("Hallo Java")
