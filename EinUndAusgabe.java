1f2a73c3f654630eb99c441631d93733b39565cf
Kai Schell <kai.schell@signet.de>
2023-02-13 14:03:37 +0100

    Simple input output with Scanner

---
+++ EinUndAusgabe.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EinUndAusgabe {
+    
+    public static void main(String[] args){
+
+        // Erzeugung des Objekts 'input' vom Datentyp 'Scanner'
+        Scanner input = new Scanner(System.in);
+        // Ausgabe, Aufforderung, etwas einzugeben
+        System.out.println("Bitte gib irgendetwas ein: ");
+        // Eingabe wird in Variable 'eingabe' gespeichert
+        String eingabe = input.nextLine(); 
+        // Ausgabe 
+        System.out.println("Du hat folgendes eingegeben: " + eingabe);
+
+    }
+    
+}
