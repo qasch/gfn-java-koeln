8605b34d69d0e726facca396c4424a66784469be
Kai Schell <kai.schell@signet.de>
2023-02-14 14:55:23 +0100

    working loop

--- EingabenPruefen.java
+++ EingabenPruefen.java
@@ -3,52 +3,57 @@ import java.util.Scanner;
 public class EingabenPruefen {
     public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
+        while(true) {
 
-        System.out.println("Bitte gib eine ganze Zahl ein: ");
+            Scanner input = new Scanner(System.in);
 
-        // Check ob zahl ein Integer ist
-        if (! input.hasNextInt())      // ! negiert den Ausdruck, wir prüfen also, ob wir KEINEN Int vorliegen haben
-        {
-            System.out.println("Das war keine Zahl, Programm wird beendet.");
-            System.exit(1);         // Programm wird mit Exit Code 1 (Fehler) beendet 
+            System.out.println("Bitte gib eine ganze Zahl ein: ");
+
+            // Check ob zahl ein Integer ist
+            if (! input.hasNextInt())      // ! negiert den Ausdruck, wir prüfen also, ob wir KEINEN Int vorliegen haben
+            {
+                System.out.println("Das war keine Zahl, probier's doch einfach noachmal: ");
+            } 
+            else 
+            {
+                int zahl = input.nextInt();
+
+                System.out.println("Du hast folgendes eingegeben: " + zahl);
+
+                // Verzweigung / If-Statement ...
+                if(zahl < 10) 
+                {
+                    System.out.println(zahl + " ist kleiner als 10");
+                } 
+                else if(zahl == 10) 
+                {
+                    System.out.println(zahl + " ist gleich 10");
+                } 
+                else if (zahl > 10 && zahl < 50)       
+                {
+                    System.out.println(zahl + " liegt zwischen 10 und 50");
+                }
+                else if (zahl == 50)       
+                {
+                    System.out.println(zahl + " ist gleich 50");
+                }
+                else if (zahl > 50 && zahl < 100)       
+                {
+                    System.out.println(zahl + " liegt zwischen 50 und 100");
+                }
+                else 
+                {
+                    System.out.println(zahl + " ist gleich oder grösser 100");
+                }
+
+                // || - logische ODER Verknüpfung
+                // && - logische UND Verknüpfung
+
+                input.close();
+                break;
+            }
         }
-
-        int zahl = input.nextInt();
-
-        System.out.println("Du hast folgendes eingegeben: " + zahl);
-
-        // Verzweigung / If-Statement ...
-        if(zahl < 10) 
-        {
-            System.out.println(zahl + " ist kleiner als 10");
-        } 
-        else if(zahl == 10) 
-        {
-            System.out.println(zahl + " ist gleich 10");
-        } 
-        else if (zahl > 10 && zahl < 50)       
-        {
-            System.out.println(zahl + " liegt zwischen 10 und 50");
-        }
-        else if (zahl == 50)       
-        {
-            System.out.println(zahl + " ist gleich 50");
-        }
-        else if (zahl > 50 && zahl < 100)       
-        {
-            System.out.println(zahl + " liegt zwischen 50 und 100");
-        }
-        else 
-        {
-            System.out.println(zahl + " ist gleich oder grösser 100");
-        }
-
-        // || - logische ODER Verknüpfung
-        // && - logische UND Verknüpfung
-
-        input.close();
-
+        
     }
     
 }
