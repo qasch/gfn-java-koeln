dfdbc3d1e73a590878bc4fba0a06e904666053e5
Kai Schell <kai.schell@signet.de>
2023-02-14 10:47:14 +0100

    switch-case weekdays

--- SwitchCase.java
+++ SwitchCase.java
@@ -15,15 +15,31 @@ public class SwitchCase {
         // switch - case
         switch (zahl) {
             case 1: 
-                System.out.println("zahl = 1");
+                System.out.println("Montag");
                 break;
             case 2: 
-                System.out.println("zahl = 2");
+                System.out.println("Dienstag");
                 break;
             case 3: 
-                System.out.println("zahl = 3");
+                System.out.println("Mittwoch");
                 break;
+            case 4: 
+                System.out.println("Donnerstag");
+                break;
+            case 5: 
+                System.out.println("Freitag");
+                break;
+            case 6: 
+                System.out.println("Samstag");
+                break;
+            case 7: 
+                System.out.println("Sonntag");
+                break;
+            default:
+                System.out.println("gibt's nischt.");
 
         }
+
+        input.close();
     }
 }
