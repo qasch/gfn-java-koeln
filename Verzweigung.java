af37b232116a742931f7de80d523548f0a765ff6
Kai Schell <kai.schell@signet.de>
2023-02-15 09:03:43 +0100

    Konditionaloperator

---
+++ Verzweigung.java
@@ -0,0 +1,22 @@
+class Verzweigung {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 5;
+        int max;
+        
+        // if(a > b) {
+        //     max = a;
+        // } else {
+        //     max = b;
+        // }       
+        // System.out.println(max);
+    
+        // max = (a > b) ? a : b;      // ? (if) : (else)
+        // System.out.println(max);
+
+        System.out.println(max = (a > b) ? a : b);
+
+    }
+}
