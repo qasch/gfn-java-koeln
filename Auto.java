92fd06192bbe6b9fb539c749ab80f115f03a5b40
Kai Schell <kai.schell@signet.de>
2023-02-16 14:35:43 +0100

    with getter and setter

--- Auto.java
+++ Auto.java
@@ -1,15 +1,15 @@
 class Auto {
 
     // Eigenschaften - Attribute - Klassenvariablen
-    String farbe;        // null
-    String treibstoffart;
-    String marke;
-    String bezeichnung;
-    int leistung;
-    int kilometerstand;  // 0
-    int geschwindigkeit; // 0
-    int tankinhalt;
-    boolean unfallfrei;  // false
+    private String farbe;        // null
+    private String treibstoffart;
+    private String marke;
+    private String bezeichnung;
+    private int leistung;
+    private int kilometerstand;  // 0
+    private int geschwindigkeit; // 0
+    private int tankinhalt;
+    private boolean unfallfrei;  // false
 
     // Standard Konstruktor
     public Auto() {
@@ -17,13 +17,60 @@ class Auto {
     }
 
     // eigener Konstruktor
-    public Auto(String bezeichnung, String farbe, int kilometerstand, int leistung, boolean unfallfrei) {
+    public Auto(String bezeichnung, String farbe, int kilometerstand, int leistung, boolean unfallfrei, int tankinhalt) {
         this.bezeichnung = bezeichnung;
-        this.farbe = farbe;
+        this.setFarbe(farbe);
         this.kilometerstand = kilometerstand;
         this.leistung = leistung;
         this.unfallfrei = unfallfrei;
+        this.setTankinhalt(tankinhalt);
     }
+
+    // Getter und Setter
+    // Getter für Attribut farbe
+    public String getFarbe() {
+        return this.farbe;
+    }
+
+    // Setter für Attribut farbe
+    public void setFarbe(String farbe) {
+        this.farbe = farbe;
+    }
+    
+    public int getLeistung(){
+        return this.leistung;
+    }
+
+    public String getBezeichnung(){
+        return this.bezeichnung;
+    }
+
+    // Getter für Attribut farbe
+    public int getTankinhalt() {
+        return this.tankinhalt;
+    }
+
+    // Setter für Attribut farbe
+    public void setTankinhalt(int liter) {
+        if(liter < 0) {
+            System.out.println("Der Tankinhalt darf nicht negativ sein.");
+        } else {
+            this.tankinhalt = liter;
+        }
+    }
+
+    public int getKilometerstand(){
+        return this.kilometerstand;
+    }
+
+    public boolean getUnfallfrei(){
+        return this.unfallfrei;
+    }
+
+    public int getGeschwindigkeit(){
+        return this.geschwindigkeit;
+    }
+
 
     // Funktionalitäten - Methoden
     void beschleunigen(int faktor){
--- AutoTest.java
+++ AutoTest.java
@@ -2,36 +2,36 @@ class AutoTest {
 
     public static void main(String[] args) {
         // Objekt vom Typ Auto erzeugen
-        Auto sportwagen = new Auto("Sportwagen", "gruen", 101, 250, true);          
+        Auto sportwagen = new Auto("Sportwagen", "gruen", 101, 250, true, 20);          
 
-        System.out.println("Unser " + sportwagen.bezeichnung + " ist " + sportwagen.farbe + 
-            " und hat eine Leistung von " + sportwagen.leistung + " PS.");
+        System.out.println("Unser " + sportwagen.getBezeichnung() + " ist " + sportwagen.getFarbe() + 
+            " und hat eine Leistung von " + sportwagen.getLeistung() + " PS.");
 
-        System.out.println("aktuelle Geschwindigkeit: " + sportwagen.geschwindigkeit);
+        System.out.println("aktuelle Geschwindigkeit: " + sportwagen.getGeschwindigkeit());
         System.out.println("Wir geben Gas!");
         sportwagen.beschleunigen(20);
-        System.out.println("aktuelle Geschwindigkeit: " + sportwagen.geschwindigkeit);
+        System.out.println("aktuelle Geschwindigkeit: " + sportwagen.getGeschwindigkeit());
 
-        if(!sportwagen.unfallfrei) {
+        if(!sportwagen.getUnfallfrei()) {
             System.out.println("Wir müssen in die Wetkstatt!");
         }
 
-        Auto lieferwagen = new Auto();          
-        lieferwagen.farbe = "weiss";
-        lieferwagen.kilometerstand = 108234;
-        lieferwagen.leistung = 75;
+        // Auto lieferwagen = new Auto();          
+        // lieferwagen.farbe = "weiss";
+        // lieferwagen.kilometerstand = 108234;
+        // lieferwagen.leistung = 75;
 
-        System.out.println("Unser " + lieferwagen.bezeichnung + " ist " + lieferwagen.farbe + 
-            " und hat eine Leistung von " + lieferwagen.leistung + " PS.");
+        // System.out.println("Unser " + lieferwagen.bezeichnung + " ist " + lieferwagen.farbe + 
+        //     " und hat eine Leistung von " + lieferwagen.leistung + " PS.");
 
-        System.out.println("aktuelle Geschwindigkeit: " + lieferwagen.geschwindigkeit);
-        System.out.println("Wir geben Gas!");
-        lieferwagen.beschleunigen(5);
-        System.out.println("aktuelle Geschwindigkeit: " + lieferwagen.geschwindigkeit);
+        // System.out.println("aktuelle Geschwindigkeit: " + lieferwagen.geschwindigkeit);
+        // System.out.println("Wir geben Gas!");
+        // lieferwagen.beschleunigen(5);
+        // System.out.println("aktuelle Geschwindigkeit: " + lieferwagen.geschwindigkeit);
 
-        if(!lieferwagen.unfallfrei) {
-            System.out.println("Wir müssen in die Wetkstatt!");
-        }
+        // if(!lieferwagen.unfallfrei) {
+        //     System.out.println("Wir müssen in die Wetkstatt!");
+        // }
 
 
     }
