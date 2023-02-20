8eefb36de2c95bbab8d257c34c3d52d4c2debff0
Kai Schell <kai.schell@signet.de>
2023-02-20 14:26:00 +0100

    placement buttons

---
+++ gui_rechner.py
@@ -0,0 +1,38 @@
+# import tkinter
+from tkinter import ttk
+import tkinter as tk
+
+def btn_close_destroy():
+    frame.destroy()
+
+def btn_calc_calc():
+    pass
+
+# Frame erstellen
+frame = tk.Tk()
+frame.title("Rechner")
+frame.geometry("800x400")
+
+# Eingabefelder
+entry_1 = tk.Entry(frame)
+entry_1.place(x=200, y = 20)
+entry_2 = tk.Entry(frame)
+entry_2.place(x=200, y = 50)
+result = tk.Entry(frame)
+result.place(x=200, y = 80)
+
+# Button erzeugen
+# ACHTUNG: die Methode, die ausgeführt werden soll, darf nicht mit Parameterklammern() aufgerufen werden
+# Sonst wird die Rückgabe der Methode in der Variablen command gespeichert, die Methode aber nicht ausgeführt
+btn_close = tk.Button(frame, text = "Close", command = btn_close_destroy)
+btn_close.place(x=200, y=120)
+
+btn_calc = tk.Button(frame, text = "Berechne", command = btn_calc_calc)
+btn_calc.place(x=300, y=120)
+
+# Endlosschleife, damit unser Frame weiter angezeigt wird
+frame.mainloop()
+
+
+
+
